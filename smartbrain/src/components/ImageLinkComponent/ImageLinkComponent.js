6d94237377669f517102ab922a5947182e013551
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 06:16:22 -0600

    setup ILC

---
+++ smartbrain/src/components/ImageLinkComponent/ImageLinkComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './ImageLinkComponent.css'
+
+const ImageLinkComponent = () => {
+  return (
+    <div className="ma4 mt0">
+    </div>
+  );
+};
+
+export default ImageLinkComponent;
