98bea1af667596348492b5135136750b7d538c7b
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 06:46:38 -0600

    fixed alignment issures

--- smartbrain/src/components/Rank/Rank.js
+++ smartbrain/src/components/Rank/Rank.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./Rank.css";
 
 const Rank = () => {
-  return (
-  );
+    return (
+      <div className="center-stack">
+        <div className="white f3">{"Miguel, your current rank is..."}</div>
+        <div className="white f2">{"#5"}</div>
+      </div>
+    );
 };
 
 export default Rank;
