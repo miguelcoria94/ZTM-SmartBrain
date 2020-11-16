49966f3ad62c692761f9671e1dbf219f58bcd093
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 03:32:00 -0600

    setting up signing proute

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -2,8 +2,43 @@ const express = require("express");
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "demo@demo.com",
+      password: "password",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "345",
+      name: "kim",
+      email: "kim@demo.com",
+      password: "password",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
+app.post('signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success')
+    } else {
+        res.status(400).json('error logging in')
+    }
+    res.json('signing')
+})
 
 
 
