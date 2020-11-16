131929258b3212756355b7dacf9c3c8a42fc262a
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 02:53:38 -0600

    testing express

--- server.js
+++ server.js
@@ -6,16 +6,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("This is the root")
+    res.status(404).send("This is the root")
 });
-
-app.get("/profile", (req, res) => {
-    res.send("getting profile");
-})
-
-app.post("/profile", (req, res) => {
-    console.log(req.body)
-    res.send('Thank you')
-})
 
 app.listen(3000);
